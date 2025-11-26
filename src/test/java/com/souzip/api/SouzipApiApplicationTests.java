ff970e9c504bc20b720d014ceb99962eaf839069
Kimdonghwan <kimdonghwan913@gmail.com>
2025-11-26 21:45:46 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/com/souzip/api/SouzipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souzip.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouzipApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

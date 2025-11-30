a9e799012348d077de32e235a0fa48a1e71910bf
bum0w0 <kgy5988@naver.com>
2025-11-30 23:00:48 +0900

    feat: ExchangeRate 레포지토리 추가

---
+++ src/main/java/com/souzip/api/domain/exchange_rate/repository/ExchangeRateRepository.java
@@ -0,0 +1,11 @@
+package com.souzip.api.domain.exchange_rate.repository;
+
+import com.souzip.api.domain.exchange_rate.entity.ExchangeRate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
+
+    boolean existsByBaseCodeAndCurrencyCode(String baseCode, String currencyCode);
+}

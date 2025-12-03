e027c17817fe4e8fe157330c39f1e69002f700c0
Kimdonghwan <kimdonghwan913@gmail.com>
2025-12-03 17:34:23 +0900

    chore: 클래스 바로 밑 필드 공백 추가

--- src/main/java/com/souzip/api/domain/country/service/CountryExternalApiClient.java
+++ src/main/java/com/souzip/api/domain/country/service/CountryExternalApiClient.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Component
 public class CountryExternalApiClient {
+
     private final RestTemplate restTemplate;
 
     @Value("${external.api.countries-base-url}")

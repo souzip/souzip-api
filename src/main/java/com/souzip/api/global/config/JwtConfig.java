33d70bd3371417a8dbae90f019feec49389d6e67
Kimdonghwan <kimdonghwan913@gmail.com>
2025-12-03 17:24:59 +0900

    feat: JWT 설정 프로퍼티 클래스 구현

---
+++ src/main/java/com/souzip/api/global/config/JwtConfig.java
@@ -0,0 +1,10 @@
+package com.souzip.api.global.config;
+
+import com.souzip.api.global.security.jwt.JwtProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(JwtProperties.class)
+public class JwtConfig {
+}

0bf5e5544714e2df16af667caa5827c6794c6004
Kimdonghwan <kimdonghwan913@gmail.com>
2025-12-10 20:16:27 +0900

    feat: 도시 모델 추가

---
+++ src/main/java/com/souzip/api/domain/city/entity/City.java
@@ -0,0 +1,52 @@
+package com.souzip.api.domain.city.entity;
+
+import com.souzip.api.domain.country.entity.Country;
+import com.souzip.api.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "city", indexes = {
+    @Index(name = "idx_city_country", columnList = "country_id"),
+    @Index(name = "idx_city_name", columnList = "name_en, name_kr")
+})
+public class City extends BaseEntity {
+
+    @Column(nullable = false)
+    private String nameEn;
+
+    @Column(nullable = false)
+    private String nameKr;
+
+    @Column(nullable = false)
+    private BigDecimal latitude;
+
+    @Column(nullable = false)
+    private BigDecimal longitude;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Country country;
+
+    public static City of(
+        String nameEn,
+        String nameKr,
+        BigDecimal latitude,
+        BigDecimal longitude,
+        Country country
+    ) {
+        return City.builder()
+            .nameEn(nameEn)
+            .nameKr(nameKr)
+            .latitude(latitude)
+            .longitude(longitude)
+            .country(country)
+            .build();
+    }
+}

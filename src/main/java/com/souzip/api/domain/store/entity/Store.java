52067d5aae365e0e71778b9864d1273104102d58
bum0w0 <kgy5988@naver.com>
2025-12-12 22:57:10 +0900

    feat: 상점 엔티티 추가

---
+++ src/main/java/com/souzip/api/domain/store/entity/Store.java
@@ -0,0 +1,33 @@
+package com.souzip.api.domain.store.entity;
+
+import com.souzip.api.domain.city.entity.City;
+import com.souzip.api.domain.country.entity.Country;
+import com.souzip.api.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+public class Store extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Country country;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "city_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private City city;
+
+    @Column(nullable = false, precision = 10, scale = 6)
+    private Double latitude;
+
+    @Column(nullable = false, precision = 10, scale = 6)
+    private Double longitude;
+
+    @Column(length = 255)
+    private String note;
+
+}

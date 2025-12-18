648388d1a1bfd127c408fe02f2b0c009c52034bf
bum0w0 <kgy5988@naver.com>
2025-12-19 00:47:25 +0900

    feat: DTO validation 추가

--- src/main/java/com/souzip/api/domain/souvenir/dto/SouvenirCreateRequest.java
+++ src/main/java/com/souzip/api/domain/souvenir/dto/SouvenirCreateRequest.java
@@ -2,23 +2,52 @@ package com.souzip.api.domain.souvenir.dto;
 
 import com.souzip.api.domain.category.entity.Category;
 import com.souzip.api.domain.souvenir.entity.Purpose;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public record SouvenirCreateRequest(
+        @NotBlank(message = "기념품 이름은 필수입니다.")
+        @Size(max = 30, message = "기념품 이름은 30자 이하여야 합니다.")
         String name,
+
+        @Positive(message = "현지 가격은 0보다 큰 값이어야 합니다.")
         Integer localPrice,
+
         String currencySymbol,
+
+        @Positive(message = "원화 가격은 0보다 큰 값이어야 합니다.")
         Integer krwPrice,
+
+        @NotBlank(message = "기념품 설명은 필수입니다.")
+        @Size(max = 1000, message = "기념품 설명은 1000자 이하여야 합니다.")
         String description,
+
+        @NotBlank(message = "주소는 필수입니다.")
         String address,
+
+        @NotBlank(message = "상세 위치는 필수입니다.")
         String locationDetail,
+
+        @NotNull(message = "위도는 필수입니다.")
         BigDecimal latitude,
+
+        @NotNull(message = "경도는 필수입니다.")
         BigDecimal longitude,
+
+        @NotNull(message = "카테고리는 필수입니다.")
         Category category,
+
+        @NotNull(message = "기념품 구매 목적은 필수입니다.")
         Purpose purpose,
+
+        @NotBlank(message = "국가 코드는 필수입니다.")
         String countryCode,
+
         List<MultipartFile> files
 ) {}

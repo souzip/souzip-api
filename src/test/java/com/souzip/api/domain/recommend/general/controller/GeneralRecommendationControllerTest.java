5d27285c5bd2ad3cf910811f2b988d79d70c8f5e
bum0w0 <kgy5988@naver.com>
2026-02-12 22:53:42 +0900

    test: 테스트 코드 수정

--- src/test/java/com/souzip/api/domain/recommend/general/controller/GeneralRecommendationControllerTest.java
+++ src/test/java/com/souzip/api/domain/recommend/general/controller/GeneralRecommendationControllerTest.java
@@ -2,11 +2,13 @@ package com.souzip.api.domain.recommend.general.controller;
 
 import com.souzip.api.docs.RestDocsSupport;
 import com.souzip.api.domain.category.entity.Category;
+import com.souzip.api.domain.recommend.general.dto.CountryRecommendationDto;
 import com.souzip.api.domain.recommend.general.dto.GeneralRecommendationDto;
 import com.souzip.api.domain.recommend.general.dto.GeneralRecommendationStatsDto;
 import com.souzip.api.domain.recommend.general.service.GeneralRecommendationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,7 +25,6 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.restdocs.payload.JsonFieldType;
 
 class GeneralRecommendationControllerTest extends RestDocsSupport {
 
@@ -34,8 +35,8 @@ class GeneralRecommendationControllerTest extends RestDocsSupport {
         return new GeneralRecommendationController(generalRecommendationService);
     }
 
-    @Test
     @DisplayName("카테고리별 추천 기념품 Top10 조회")
+    @Test
     void getCategoryTop10() throws Exception {
         // given
         List<GeneralRecommendationDto> responseList = IntStream.rangeClosed(1, 10)
@@ -52,11 +53,11 @@ class GeneralRecommendationControllerTest extends RestDocsSupport {
                 .willReturn(responseList);
 
         // when & then
-        mockMvc.perform(get("/api/discovery/general/category/FOOD_SNACK"))
+        mockMvc.perform(get("/api/discovery/categories/FOOD_SNACK/souvenirs"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data[0].id").value(1))
-                .andDo(document("recommend/general/category-top10",
+                .andDo(document("recommend/category-top10",
                         getDocumentRequest(),
                         getDocumentResponse(),
                         apiResponseFields(
@@ -71,8 +72,9 @@ class GeneralRecommendationControllerTest extends RestDocsSupport {
                 ));
     }
 
-    @Test
+    @Deprecated
     @DisplayName("나라별 추천 기념품 Top10 조회")
+    @Test
     void getCountryTop10() throws Exception {
         // given
         List<GeneralRecommendationDto> responseList = IntStream.rangeClosed(1, 10)
@@ -80,7 +82,7 @@ class GeneralRecommendationControllerTest extends RestDocsSupport {
                         (long) i,
                         "기념품 " + i,
                         Category.SOUVENIR_BASIC,
-                        "JP",
+                        "KR",
                         "https://example.com/thumb" + i + ".jpg"
                 ))
                 .collect(Collectors.toList());
@@ -89,12 +91,12 @@ class GeneralRecommendationControllerTest extends RestDocsSupport {
                 .willReturn(responseList);
 
         // when & then
-        mockMvc.perform(get("/api/discovery/general/country/KR"))
+        mockMvc.perform(get("/api/discovery/countries/KR/souvenirs"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data[0].id").value(1))
                 .andExpect(jsonPath("$.data[0].name").value("기념품 1"))
-                .andDo(document("recommend/general/country-top10",
+                .andDo(document("recommend/country-top10",
                         getDocumentRequest(),
                         getDocumentResponse(),
                         apiResponseFields(
@@ -109,8 +111,9 @@ class GeneralRecommendationControllerTest extends RestDocsSupport {
                 ));
     }
 
+    @Deprecated
+    @DisplayName("기념품 최다등록 국가 Top10 조회(통계만)")
     @Test
-    @DisplayName("기념품 최다등록 국가 Top10 조회")
     void getTopCountriesTop10() throws Exception {
         // given
         List<GeneralRecommendationStatsDto> statsList = List.of(
@@ -130,7 +133,7 @@ class GeneralRecommendationControllerTest extends RestDocsSupport {
                 .willReturn(statsList);
 
         // when & then
-        mockMvc.perform(get("/api/discovery/general/countries"))
+        mockMvc.perform(get("/api/discovery/countries/top10"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data").isArray())
@@ -138,7 +141,7 @@ class GeneralRecommendationControllerTest extends RestDocsSupport {
                 .andExpect(jsonPath("$.data[0].countryCode").value("JP"))
                 .andExpect(jsonPath("$.data[1].countryCode").value("US"))
                 .andExpect(jsonPath("$.data[2].countryCode").value("TW"))
-                .andDo(document("recommend/general/stats-top10",
+                .andDo(document("recommend/stats-top10",
                         getDocumentRequest(),
                         getDocumentResponse(),
                         apiResponseFields(
@@ -151,29 +154,30 @@ class GeneralRecommendationControllerTest extends RestDocsSupport {
                 ));
     }
 
+    @Deprecated
+    @DisplayName("기념품 최다등록 국가 Top3 조회(통계만)")
     @Test
-    @DisplayName("기념품 최다등록 국가 Top3 통계 조회")
-    void getTopCountriesThisMonth() throws Exception {
+    void getTopCountriesTop3() throws Exception {
         // given
-        GeneralRecommendationStatsDto stat1 = new GeneralRecommendationStatsDto("JP", "일본", 20L);
-        GeneralRecommendationStatsDto stat2 = new GeneralRecommendationStatsDto("US", "미국", 15L);
-        GeneralRecommendationStatsDto stat3 = new GeneralRecommendationStatsDto("TW", "대만", 10L);
-
-        List<GeneralRecommendationStatsDto> statsList =
-                List.of(stat1, stat2, stat3);
+        List<GeneralRecommendationStatsDto> statsList = List.of(
+                new GeneralRecommendationStatsDto("JP", "일본", 20L),
+                new GeneralRecommendationStatsDto("US", "미국", 15L),
+                new GeneralRecommendationStatsDto("TW", "대만", 10L)
+        );
 
         given(generalRecommendationService.getTop3CountriesBySouvenirCount())
                 .willReturn(statsList);
 
         // when & then
-        mockMvc.perform(get("/api/discovery/general/stats"))
+        mockMvc.perform(get("/api/discovery/countries/top3"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data[0].countryNameKr").value("일본"))
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data.length()").value(3))
                 .andExpect(jsonPath("$.data[0].countryCode").value("JP"))
                 .andExpect(jsonPath("$.data[1].countryCode").value("US"))
                 .andExpect(jsonPath("$.data[2].countryCode").value("TW"))
-                .andDo(document("recommend/general/stats-top3",
+                .andDo(document("recommend/stats-top3",
                         getDocumentRequest(),
                         getDocumentResponse(),
                         apiResponseFields(
@@ -181,6 +185,65 @@ class GeneralRecommendationControllerTest extends RestDocsSupport {
                                 fieldWithPath("data[].countryNameKr").type(JsonFieldType.STRING).description("나라명 (한글)"),
                                 fieldWithPath("data[].countryCode").type(JsonFieldType.STRING).description("국가 코드"),
                                 fieldWithPath("data[].souvenirCount").type(JsonFieldType.NUMBER).description("등록된 기념품 개수 (integer)"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지").optional()
+                        )
+                ));
+    }
+
+    @DisplayName("기념품 최다등록 Top10 국가 + 국가별 기념품 Top10 조회")
+    @Test
+    void getTopCountriesWithTop10Souvenirs() throws Exception {
+        // given
+        List<GeneralRecommendationDto> jpSouvenirs = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> new GeneralRecommendationDto(
+                        200L + i,
+                        "일본 기념품 " + i,
+                        Category.SOUVENIR_BASIC,
+                        "JP",
+                        "https://example.com/jp/thumb" + i + ".jpg"
+                ))
+                .collect(Collectors.toList());
+
+        List<GeneralRecommendationDto> usSouvenirs = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> new GeneralRecommendationDto(
+                        300L + i,
+                        "미국 기념품 " + i,
+                        Category.SOUVENIR_BASIC,
+                        "US",
+                        "https://example.com/us/thumb" + i + ".jpg"
+                ))
+                .collect(Collectors.toList());
+
+        List<CountryRecommendationDto> response = List.of(
+                new CountryRecommendationDto("JP", "일본", 20L, jpSouvenirs),
+                new CountryRecommendationDto("US", "미국", 15L, usSouvenirs)
+        );
+
+        given(generalRecommendationService.getTopCountriesWithTop10Souvenirs())
+                .willReturn(response);
+
+        // when & then
+        mockMvc.perform(get("/api/discovery/countries/top10/souvenirs"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data[0].countryCode").value("JP"))
+                .andExpect(jsonPath("$.data[0].souvenirs").isArray())
+                .andExpect(jsonPath("$.data[0].souvenirs.length()").value(10))
+                .andDo(document("recommend/countries-top10-souvenirs-top10",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        apiResponseFields(
+                                fieldWithPath("data").type(JsonFieldType.ARRAY).description("기념품 최다등록 국가 Top10 및 국가별 기념품 Top10"),
+                                fieldWithPath("data[].countryCode").type(JsonFieldType.STRING).description("국가 코드"),
+                                fieldWithPath("data[].countryNameKr").type(JsonFieldType.STRING).description("나라명 (한글)"),
+                                fieldWithPath("data[].souvenirCount").type(JsonFieldType.NUMBER).description("등록된 기념품 개수 (integer)"),
+                                fieldWithPath("data[].souvenirs").type(JsonFieldType.ARRAY).description("해당 국가의 추천 기념품 리스트(최대 10개)"),
+                                fieldWithPath("data[].souvenirs[].id").type(JsonFieldType.NUMBER).description("기념품 ID (integer)"),
+                                fieldWithPath("data[].souvenirs[].name").type(JsonFieldType.STRING).description("기념품 이름"),
+                                fieldWithPath("data[].souvenirs[].category").type(JsonFieldType.STRING).description("기념품 카테고리"),
+                                fieldWithPath("data[].souvenirs[].countryCode").type(JsonFieldType.STRING).description("기념품 국가 코드"),
+                                fieldWithPath("data[].souvenirs[].thumbnailUrl").type(JsonFieldType.STRING).description("대표 이미지 URL"),
                                 fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지").optional()
                         )
                 ));

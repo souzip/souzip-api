0f17c7923f44be9e04bbac8949003e931f11df52
Kimdonghwan <kimdonghwan913@gmail.com>
2026-02-14 11:22:13 +0900

    refactor: SecurityConfig에 명시적인 AccessDeniedHandler 추가

--- src/main/java/com/souzip/api/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/souzip/api/global/exception/GlobalExceptionHandler.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -80,17 +79,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity
             .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
             .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR.getMessage()));
-    }
-
-    @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e) {
-        ensureTraceId();
-
-        log.error("[ACCESS-DENIED-ERROR] message={}", e.getMessage());
-
-        return ResponseEntity
-            .status(ErrorCode.FORBIDDEN.getStatus())
-            .body(ErrorResponse.of(ErrorCode.FORBIDDEN.getMessage()));
     }
 
     private void ensureTraceId() {

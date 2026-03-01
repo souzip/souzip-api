0a49279f5334461d4153d0108708ad048bc2497b
Kimdonghwan <kimdonghwan913@gmail.com>
2026-03-01 16:12:59 +0900

    test: 리버스 지오코딩 서비스 테스트 작성

--- src/main/java/com/souzip/api/application/location/ReverseGeocodingService.java
+++ src/main/java/com/souzip/api/application/location/ReverseGeocodingService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class GeocodingService implements ReverseGeocoding {
+public class ReverseGeocodingService implements ReverseGeocoding {
 
     private final AddressProvider addressProvider;
 

c2fab8e684fa4546791b1a5ded32d1534ce7c690
Kimdonghwan <kimdonghwan913@gmail.com>
2026-03-11 21:38:51 +0900

    fix: 어드민 관련 파일 수정

--- src/main/java/com/souzip/application/admin/required/CityQueryPort.java
+++
@@ -1,9 +0,0 @@
-package com.souzip.application.admin.required;
-
-import com.souzip.domain.city.entity.City;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface CityQueryPort {
-    Page<City> getCities(Long countryId, String keyword, Pageable pageable);
-}

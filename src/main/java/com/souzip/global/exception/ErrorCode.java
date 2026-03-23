228d974d3aa7ce919be86fa17ebfb8d8d1aebe8e
bum0w0 <kgy5988@naver.com>
2026-03-23 21:06:22 +0900

    feat: 찜 관련 에러코드 추가

--- src/main/java/com/souzip/global/exception/ErrorCode.java
+++ src/main/java/com/souzip/global/exception/ErrorCode.java
@@ -43,6 +43,9 @@ public enum ErrorCode implements BaseErrorCode {
     SOUVENIR_NOT_FOUND(HttpStatus.NOT_FOUND, "기념품을 찾을 수 없습니다."),
     INVALID_SOUVENIR_LOCATION(HttpStatus.BAD_REQUEST, "기념품의 위치 정보가 유효하지 않습니다."),
 
+    WISHLIST_NOT_FOUND(HttpStatus.NOT_FOUND, "찜 목록에 없는 기념품입니다."),
+    ALREADY_WISHLISTED(HttpStatus.BAD_REQUEST, "이미 찜한 기념품입니다."),
+
     FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
     FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, "파일 크기가 50MB를 초과합니다."),
     INVALID_FILE_TYPE(HttpStatus.BAD_REQUEST, "허용되지 않은 파일 형식입니다."),
